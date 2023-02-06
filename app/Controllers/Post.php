c68867c733421ec7dad81bd15a0a372a7d9a5052
adrianhhd <adrian@globalindointimates.com>
2023-02-06 07:20:55 +0700

    Post.php

--- app/Controllers/Post.php
+++ app/Controllers/Post.php
@@ -6,8 +6,8 @@ use CodeIgniter\RESTful\ResourceController;
 
 class Post extends ResourceController
 {
-    protected $modelName = 'App\Models\PostModel';
-    protected $format = 'json';
+    protected $modelName = 'App\Models\PostModel'; //model yang akan digunakan di controller ini
+    protected $format = 'json'; // return format data
 
     /**
      * index function
@@ -24,7 +24,7 @@ class Post extends ResourceController
 
     /**
      * show function
-     * @method : GET with params ID
+     * @method : GET with params ID (untuk edit)
      */
     public function show($id = null)
     {
@@ -38,6 +38,73 @@ class Post extends ResourceController
     public function create()
     {
         if ($this->request) {
+            //get request from Vue Js
+            //Hasil dari reques vue json adalah berupa json
+            if ($this->request->getJSON()) {
+                $json = $this->request->getJSON();
+                $post = $this->model->insert([
+                    'title' => $json->title,
+                    'content' => $json->content
+                ]);
+            } else {
+                //get request from Postman and more
+                $post = $this->model->insert([
+                    'title' => $this->request->getPost('title'),
+                    'content' => $this->request->getPost('content')
+                ]);
+            }
+
+            return $this->respond([
+                'statusCode' => 201,
+                'content' => 'Data Berhasil Disimpan'
+            ], 201);
+        }
+    }
+
+    /**
+     * update function
+     * @method : PUT or PATCH
+     */
+    public function update($id = null)
+    {
+        //model 
+        $post = $this->model;
+
+        if ($this->request) {
+            //get request from Vue Js
+            if ($this->request->getJSON()) {
+                $json = $this->request->getJSON();
+                $post->update($json->id, [
+                    'title' => $json->title,
+                    'content' => $json->content
+                ]);
+            } else {
+                //get request from Postman and more
+                $data = $this->request->getRawInput();
+                $post->update($id, $data);
+            }
+
+            return $this->respond([
+                'statusCode' => 200,
+                'message' => 'Data Behasil Diupdate!'
+            ], 200);
+        }
+    }
+
+    /**
+     * edit function
+     * @method : DELETE with params ID
+     */
+    public function delete($id = null)
+    {
+        $post = $this->model->find($id);
+        if ($post) {
+            $this->model->delete($id);
+
+            return $this->respond([
+                'statusCode' => 200,
+                'message' => 'Data Berhasil Dihapus!'
+            ], 200);
         }
     }
 }
